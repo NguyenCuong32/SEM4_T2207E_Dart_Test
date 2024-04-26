05b5be4ae3d8402a91f0fb2e859eb95322f2f084
young <youngspidey2301@gmail.com>
2024-04-26 08:39:26 +0700

    j

---
+++ my_examp/bin/my_examp.dart
@@ -0,0 +1,5 @@
+import 'package:my_examp/my_examp.dart' as my_examp;
+
+void main(List<String> arguments) {
+  print('Hello world: ${my_examp.calculate()}!');
+}
---
+++ my_examp/test/my_examp_test.dart
@@ -0,0 +1,8 @@
+import 'package:my_examp/my_examp.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
